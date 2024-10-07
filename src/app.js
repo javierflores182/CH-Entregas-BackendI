95f45afa34f60a69005cad96cf8a5367025a4bfa
javierflores182 <javierflores182@gmail.com>
2024-10-07 14:27:20 -0600

    Entregable - Avance 1

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
-// import express from 'express';
-// import productsRouter from './routes/products.router.js'
-
-// const app = express();
-// const PORT = 9090;
-
-// //Preparar la configuracion del servidor para recibir objetos JSON.
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-
-// // Routes
-// app.use("/api/products", productsRouter)
-
-
-// app.listen(PORT, () => {
-//     console.log("listening on port: " + PORT);
-// });
-
 import express from 'express';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.js';  // Importa el router de carritos
